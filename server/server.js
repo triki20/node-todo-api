35d9bd817823eaec2a034c8247dc38be2fecd5eb
yahalom <triki20@gmail.com>
2019-01-09 16:53:13 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,15 @@ app.post('/todos' , (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todo) => {
+        res.send({todo});
+    },
+     (e) => {
+         res.status(400).send(e)
+     });
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
