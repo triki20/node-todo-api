2989adcc7fa0b35b924aa0d5f6b6a5bfab0c4157
yahalom <triki20@gmail.com>
2019-01-10 13:23:39 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Libraries
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 // Local models
 var {mongoose} = require('./DB/mongoose');
@@ -33,6 +34,21 @@ app.get('/todos', (req, res) => {
      });
 });
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+      res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+      if(!todo){
+          return res.status(404).send();
+      }
+      res.status(200).send(todo);
+  }).catch((e) => res.status(400).send());
+
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
