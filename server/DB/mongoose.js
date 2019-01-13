3cbdb998b59c1046abe55cfb5ca7904167368002
yahalom <triki20@gmail.com>
2019-01-13 11:55:38 +0200

    Setup app for heroku test 2

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://triki20:Aa250990!@ds129593.mlab.com:29593/todoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose 
