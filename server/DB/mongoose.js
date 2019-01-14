5f657b2a80ff7f71e69b5b2656d4f3a50bfd16fc
yahalom <triki20@gmail.com>
2019-01-14 19:36:12 +0200

    Update moongose file

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb:// <USER> : <PASSWORD> @ds129593.mlab.com:29593/todoapp' || 'mongodb://localhost:27017/TodoApp');
+var mongoLocal = 'mongodb://localhost:27017/TodoApp';
+
+mongoose.connect( process.env.MONGODB_URI || mongoLocal, {'useNewUrlParser': true});
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
 module.exports = {
     mongoose 
 }
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
+
 
 // Local models
 var {mongoose} = require('./DB/mongoose');
@@ -64,6 +66,33 @@ app.delete('/todos/:id', (req,res) => {
         }
         res.status(200).send({todo});
     }).catch((e) => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(res.body, ['text','completed']);
+
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set:body}, {$new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send()
+    })
+
+
 });
 
 app.listen(port, () => {
