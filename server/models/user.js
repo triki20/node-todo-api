b0e7271e4ae191bafde300ada7a5e7d62cadba01
yahalom <triki20@gmail.com>
2019-01-27 13:32:34 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -71,6 +71,26 @@ UserSchema.statics.findByTokens = function (token) {
       'tokens.access': 'auth'
     });
   };
+  
+  UserSchema.statics.findByCredentials = function (email, password){
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(!res){
+                    reject();
+                }else{
+                    resolve(user);
+                }
+            });
+        });
+    });
+  };
 
 UserSchema.pre('save', function(next) {
     var user = this;
